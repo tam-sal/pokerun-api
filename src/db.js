efd087e1ad9a1a6d3ebe37413f3411fc2083f3f6
tam-sal <t.saleh.gc@gmail.com>
2023-03-22 08:27:29 -0300

    Authenticating db

--- src/db.js
+++ src/db.js
@@ -10,8 +10,21 @@ const { DB_USER, DB_PASS, DB_NAME, DB_PORT, DB_HOST } = process.env
 //* CREATE A SEQUELIZE INSTANCE
 const database = new Sequelize(
   `postgres://pokemon_5mgu_user:ZV3VXz8qCnG4nywHAdZy2gDigFsKCh1z@dpg-cgde40t269v52g76hc8g-a/pokemon_5mgu`,
-  { logging: false, native: false }
+  {
+    logging: false, native: false, dialect: 'postgres',
+    dialectOptions: {
+      ssl: true
+    }
+  }
 )
+database
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
 
 
 //* MODELS INJECTION - Connect Model to DB and Create its corresponding table 
