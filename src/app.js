2a9239a7472fcb47a6c3d506b43f660d93bbb0c7
tam-sal <t.saleh.gc@gmail.com>
2023-04-29 04:07:19 -0300

    deploy

--- src/app.js
+++ src/app.js
@@ -2,16 +2,17 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const mainRouter = require('./routes/mainRouter')
+require('dotenv').config()
 
 
 //* INSTANTIATING APP
 const app = express()
-
-const origins = { origin: 'https://pokerun.onrender.com' }
+const { allowed_origins } = process.env
+const origins = { origin: allowed_origins }
 
 //* MIDDLEWARES
 app.use(express.json())
-app.use(cors())
+app.use(cors(origins))
 app.use(morgan('dev'))
 //* Sanity check
 
